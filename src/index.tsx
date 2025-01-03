7e149e747634e60df39cf559af9110ea9f1ebf0f
Vitaly Karpov <vitvit0312@gmail.com>
2025-01-04 03:43:38 +0500

    Create store

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,21 @@ import ReactDOM from 'react-dom/client';
 import {App} from './components/app/app.tsx';
 import {OFFER_LIST_MOCK, OFFER_DETAILED_MOCKS} from './mocks/offers.ts';
 import {REVIEW_LIST_MOCK} from './mocks/reviews.ts';
+import {Provider} from 'react-redux';
+import {store} from './store';
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+
 root.render(
-  <App
-    offerList={OFFER_LIST_MOCK}
-    offersDetailed={OFFER_DETAILED_MOCKS}
-    reviewList={REVIEW_LIST_MOCK}
-  />
+  <Provider store={store}>
+    <App
+      offerList={OFFER_LIST_MOCK}
+      offersDetailed={OFFER_DETAILED_MOCKS}
+      reviewList={REVIEW_LIST_MOCK}
+    />
+  </Provider>
 );
