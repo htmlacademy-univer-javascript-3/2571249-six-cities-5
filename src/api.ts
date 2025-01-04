648ede0571d81f0d474708d87ba0c94c2bd49022
Vitaly Karpov <vitvit0312@gmail.com>
2025-01-04 06:12:57 +0500

    Delete mocks and add API

---
+++ src/api.ts
@@ -0,0 +1,9 @@
+﻿import axios, {AxiosInstance} from 'axios';
+
+const BASE_URL = 'https://14.design.htmlacademy.pro/six-cities';
+const TIMEOUT = 5000;
+
+export const CreateAPI = (): AxiosInstance => axios.create({
+  baseURL: BASE_URL,
+  timeout: TIMEOUT,
+});
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,16 @@
 ﻿import {configureStore} from '@reduxjs/toolkit';
 import {reducer} from './reducer.ts';
+import {CreateAPI} from '../api.ts';
+
+
+const api = CreateAPI();
 
 
 export const store = configureStore({
   reducer: reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    thunk: {
+      extraArgument: api,
+    },
+  }),
 });
